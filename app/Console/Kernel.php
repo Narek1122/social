65544feaf6186832fccf8d4571f8612deab66b85
Narek <narek110022@ru>
2021-02-22 20:44:42 +0400

    2.22.21

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,8 +2,11 @@
 
 namespace App\Console;
 
+use App\User;
+use Carbon\Carbon;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Console\Commands\DeleteUsers;
 
 class Kernel extends ConsoleKernel
 {
@@ -24,7 +27,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+      $schedule->command(DeleteUsers::class)->daily();
     }
 
     /**
